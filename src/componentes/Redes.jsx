c3dcf0bc01826ffb6e6ef6874597be5fbf2ff3df
nhoelya <noeliafalkiewicz@gmail.com>
2023-07-24 11:50:38 -0300

    modificacion redes

--- src/componentes/Redes.jsx
+++ src/componentes/Redes.jsx
@@ -10,30 +10,36 @@ function Redes () {
     return (
 
         <div className='contenedorRedesyFormulario'>
-            <div className='bloque container text-center'>
+            <div className='bloque container text-start'>
+
                <div className='datos'>
-                   <img className='log' src={whatsapp} />
-                   <p className='info'><strong>+12345678910</strong></p>
-                </div>
-                <div className='datos'>
-                     <img className='log' src={facebook} />
-                     <p className='info'><strong>Regina Falkiewicz</strong></p>
-                </div>
-                <div className='datos row align-items-center '>
+                  <span><a href=''>
+                         <img className='log' src={whatsapp} />
+                         <p className='info'><strong>+12345678910</strong></p>
+                  </a></span>
+               </div>
+
+               <div className='datos'>
+                  <span><a href='https://www.facebook.com/noeliareg?mibextid=ZbWKwL' target='_blank'>
+                      <img className='log' src={facebook} />
+                       <p className='info'><strong>Regina Falkiewicz</strong></p>
+                  </a></span>
+               </div>
+
+               <div className='datos'>
                      <span><a href='https://www.linkedin.com/in/noelia-falkiewicz' target='_blank'>
-                        <div className='col-2'>
-                            <img className='log' src={linkedin}  />
-                        </div>
-                        <div className='col-4'>
-                           <p className='info' ><strong>Noelia Falkiewicz</strong></p>
-                        </div>
+                           <img className='log' src={linkedin}  />
+                           <p className='info' ><strong>Noelia Falkiewicz</strong></p>  
                      </a></span>
-                   
-                </div>
-                <div className='datos'>
-                   <img className='log' src={instagram} />
-                   <p className='info'><strong>@noilita_</strong></p>
-                </div>
+               </div>
+
+               <div className='datos'>
+                    <span><a href='https://instagram.com/noilita_?igshid=MzNlNGNkZWQ4Mg==' target='_blank'>
+                        <img className='log' src={instagram} />
+                        <p className='info'><strong>@noilita_</strong></p>
+                    </a></span>  
+               </div>
+               
            </div>
            
         </div>
